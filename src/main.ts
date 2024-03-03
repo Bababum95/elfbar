859b269fa24c9684278c588726d911a56a16e297
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-03-03 07:56:42 +0700

    convert webm to webp

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,6 @@
 /**
- * Hide video and fallback image on error
+ * Add Roboto fonts
  */
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
-const video = document.querySelector(
-	'.delivery__item_free-video'
-) as HTMLVideoElement;
-const fallbackImg = document.querySelector(
-	'.delivery__item_free-fallback'
-) as HTMLImageElement;
-
-video.addEventListener('error', () => {
-	video.style.display = 'none';
-	fallbackImg.style.display = 'block';
-});
-
-video.addEventListener('loadeddata', () => {
-	fallbackImg.style.display = 'none';
-});
