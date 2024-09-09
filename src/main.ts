d6061bc67ff7246eb5358a343c2c5ae205395302
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-09-09 23:17:29 +0700

    "version": "1.3.0"

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,14 @@
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+const popup = document.querySelector('.popup');
+const buttons = document.querySelectorAll('.popup__button');
+
+buttons[0].addEventListener('click', () => {
+  window.location.href = 'https://www.google.com';
+});
+
+buttons[1].addEventListener('click', () => {
+  popup?.classList.add('close');
+});
