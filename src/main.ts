2b8c97515cdea7eaf3ea7714e0600c18ba3997e7
Andrei <116939459+Bababum95@users.noreply.github.com>
2024-09-12 10:26:50 +0700

    "version": "1.3.1"

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 /**
- * Add Roboto fonts
+ * Add Inter fonts
  */
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 
 const popup = document.querySelector('.popup');
 const buttons = document.querySelectorAll('.popup__button');
 
 buttons[0].addEventListener('click', () => {
-  window.location.href = 'https://www.google.com';
+  window.location.href =
+    'https://glimp.de/kategorie/einweg-e-zigaretten/elf-bar/elf-bar-600/?_gl=1*12ijvoi*_ga*mtuyntk2otuyos4xnzi1odyxnde5*_ga_9mfz1tfhsb*mtcyntg2mtqxos4xljaumtcyntg2mtqxos4wljauma..';
 });
 
 buttons[1].addEventListener('click', () => {
   popup?.classList.add('close');
+  window.location.href =
+    'https://glimp.de/kategorie/einweg-e-zigaretten/elf-bar/elf-bar-600/?_gl=1*12ijvoi*_ga*mtuyntk2otuyos4xnzi1odyxnde5*_ga_9mfz1tfhsb*mtcyntg2mtqxos4xljaumtcyntg2mtqxos4wljauma..';
 });
--- vite.config.ts
+++ vite.config.ts
@@ -7,31 +7,31 @@ const SRC_PATH = resolve(__dirname, 'src');
 const BASE_PATH = process.env.NODE_ENV === 'pages' ? '/elfbar/' : '/';
 
 export default defineConfig({
-	base: BASE_PATH,
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(SRC_PATH, 'main.ts'),
-				styles: resolve(SRC_PATH, 'styles.scss'),
-				index: resolve(__dirname, 'index.html'),
-			},
-		},
-		outDir: 'dist',
-		assetsDir: 'assets',
-		emptyOutDir: true,
-		minify: true,
-		manifest: false,
-	},
-	plugins: [
-		ViteImageOptimizer({
-			test: /\.(jpe?g|png|gif|tiff|svg|avif)$/i,
-		}),
-		createHtmlPlugin({
-			minify: true,
-			template: resolve(__dirname, 'index.html'),
-		}),
-	],
-	optimizeDeps: {
-		include: ['@fontsource/roboto'],
-	},
+  base: BASE_PATH,
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(SRC_PATH, 'main.ts'),
+        styles: resolve(SRC_PATH, 'styles.scss'),
+        index: resolve(__dirname, 'index.html'),
+      },
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    minify: true,
+    manifest: false,
+  },
+  plugins: [
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|svg|avif)$/i,
+    }),
+    createHtmlPlugin({
+      minify: true,
+      template: resolve(__dirname, 'index.html'),
+    }),
+  ],
+  optimizeDeps: {
+    include: ['@fontsource/roboto'],
+  },
 });
